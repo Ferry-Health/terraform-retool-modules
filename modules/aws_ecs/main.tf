38def22e681f69b3003b4e98908f07c7d6de9d22
Andrew Buie <buie.andrew@gmail.com>
2024-02-15 16:27:30 -0800

    encrypt storage and enable backups

--- modules/aws_ecs/main.tf
+++ modules/aws_ecs/main.tf
@@ -40,6 +40,8 @@ resource "aws_db_instance" "this" {
 
   skip_final_snapshot = true
   apply_immediately   = true
+  storage_encrypted   = true
+  backup_retention_period = 7
 }
 
 resource "aws_ecs_service" "retool" {
